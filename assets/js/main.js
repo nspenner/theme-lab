19e9f4ece46725facfb899e3ffe6c2a76259f12d
Nathan Minchow <nathan.minchow@outlook.com>
2020-02-18 23:50:12 -0600

    add theme support

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,45 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+const darkTheme = {
+  "--accent-color": "#4acaa8",
+  "--background-color": "#343737",
+  "--active-scroll-background": "#f0ffff",
+	"--color-text": "white",
+	"--sidebar-color": "#444c48"
+};
+
+const lightTheme = {
+  "--accent-color": "#4acaa8",
+  "--background-color": "#f0ffff",
+  "--active-scroll-background": "#f0ffff",
+	"--color-text": "#777",
+	"--sidebar-color": "#4acaa8"
+};
+
+let currentTheme = lightTheme;
+
+function applyTheme(theme) {
+  let root = document.documentElement;
+  root.style.setProperty("--accent-color", theme["--accent-color"]);
+  root.style.setProperty("--background-color", theme["--background-color"]);
+  root.style.setProperty(
+    "--active-scroll-background",
+    theme["--active-scroll-background"]
+  );
+  root.style.setProperty("--color-text", theme["--color-text"]);
+  root.style.setProperty("--sidebar-color", theme["--sidebar-color"]);
+  currentTheme = theme;
+}
+
+function toggleTheme() {
+  if (currentTheme == lightTheme) {
+    applyTheme(darkTheme);
+  } else {
+    applyTheme(lightTheme);
+  }
+}
+
 (function($) {
 
 	var $window = $(window),
